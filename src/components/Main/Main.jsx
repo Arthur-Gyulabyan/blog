71378cfb6c6faca1b4456a21e23f23a171a2dbe1
Arthur <gyulabyana@gmail.com>
2021-07-25 23:14:12 +0400

    feat(Main): add Main component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import NavBar from '../NavBar/NavBar';
+import LogIn from '../LogIn/LogIn';
+
+export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+
+  render() {
+    const { isLoggedIn } = this.state;
+
+    return (
+      <>
+        <Router>
+          <NavBar isLoggedIn={isLoggedIn} />
+
+          <Switch>
+            <Route path="/login">
+              <LogIn />
+            </Route>
+          </Switch>
+        </Router>
+      </>
+    );
+  }
+}
