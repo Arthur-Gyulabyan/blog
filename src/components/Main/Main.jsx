353f7b23cc7f7f040bc21c78b790b63aa3c56b1a
Arthur <gyulabyana@gmail.com>
2021-07-26 03:58:54 +0400

    feat(Main): add route to Posts

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,24 +2,54 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
 import LogIn from '../LogIn/LogIn';
+import Posts from '../Posts/Posts';
 
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoggedIn: false,
+      posts: [
+        {
+          author: 'Arthur',
+          title: 'Hello',
+          content: 'May the force be with you!',
+          date: new Date().toDateString(),
+        },
+        {
+          author: 'Hayk',
+          title: 'Bye',
+          content: 'Hastala-Vista Baby',
+          date: new Date().toDateString(),
+        },
+        {
+          author: 'Eminem',
+          title: 'Sing For The Moment',
+          content: 'Bla Bla Bla',
+          date: new Date().toDateString(),
+        },
+        {
+          author: 'Mad Max',
+          title: 'Max Rockatansky',
+          content: 'Bla Bla Bla',
+          date: new Date().toDateString(),
+        },
+      ],
     };
   }
 
   render() {
-    const { isLoggedIn } = this.state;
+    const { isLoggedIn, posts } = this.state;
 
     return (
       <>
         <Router>
           <NavBar isLoggedIn={isLoggedIn} />
-
           <Switch>
+            <Route exact path="/">
+              <Posts posts={posts} />
+            </Route>
+
             <Route path="/login">
               <LogIn />
             </Route>
