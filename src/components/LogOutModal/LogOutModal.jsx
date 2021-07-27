0ec75733de0aa3f0a2de5382034829cac6ca67d3
Arthur <gyulabyana@gmail.com>
2021-07-28 00:44:33 +0400

    feat(LogOutModal): set final styles of modal

--- src/components/LogOutModal/LogOutModal.jsx
+++ src/components/LogOutModal/LogOutModal.jsx
@@ -2,25 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { createTheme, withStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
+import { pink } from '@material-ui/core/colors';
 
 const theme = createTheme();
 
 const styles = {
   paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
     position: 'absolute',
-    width: 400,
+    top: '25%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 350,
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
+    padding: theme.spacing(1, 4, 3),
+  },
+  centeredText: {
+    textAlign: 'center',
+  },
+  approveButton: {
+    backgroundColor: pink['600'],
+    color: '#fafafa',
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.main,
+    },
   },
 };
 
 function LogOutModal({ classes, handleClose, isOpen }) {
   const body = (
     <div className={classes.paper}>
-      <h2 id="simple-modal-title">Text in a modal</h2>
-      <p id="simple-modal-description">Arthur Gyulabyan</p>
+      <h2 id="simple-modal-title" className={classes.centeredText}>
+        Are you sure?
+      </h2>
+      <Button className={classes.approveButton}>Yes</Button>
     </div>
   );
 
@@ -40,6 +59,8 @@ function LogOutModal({ classes, handleClose, isOpen }) {
 LogOutModal.propTypes = {
   classes: PropTypes.shape({
     paper: PropTypes.string.isRequired,
+    centeredText: PropTypes.string.isRequired,
+    approveButton: PropTypes.string.isRequired,
   }).isRequired,
   handleClose: PropTypes.func.isRequired,
   isOpen: PropTypes.bool.isRequired,
