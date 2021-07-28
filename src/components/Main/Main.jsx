7a68214aead731d8bf5ed1bbdd42eb7481b43306
Arthur <gyulabyana@gmail.com>
2021-07-28 20:16:12 +0400

    feat(Main): add log in and log out functionality

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -14,10 +14,18 @@ export default class Main extends React.Component {
     this.state = {
       isLoggedIn: false,
       posts: getData('posts') ?? [],
+      currentUser: {
+        name: '',
+        userId: '',
+      },
     };
   }
 
   addPost = (title, content) => {
+    const {
+      currentUser: { name },
+    } = this.state;
+
     if (title && content) {
       this.setState((prevState) => {
         const newPosts = [
@@ -25,7 +33,7 @@ export default class Main extends React.Component {
           {
             title,
             content,
-            author: 'Arthur Gyulabyan',
+            author: name,
             date: new Date().toDateString(),
             id: generateUniqueID(),
           },
@@ -48,23 +56,40 @@ export default class Main extends React.Component {
     this.setState({ posts: newPosts });
   };
 
+  handleLogIn = (name, userId) => {
+    this.setState({
+      isLoggedIn: true,
+      currentUser: {
+        name,
+        userId,
+      },
+    });
+  };
+
+  handleLogOut = () => {
+    this.setState({ isLoggedIn: false, currentUser: {} });
+  };
+
   render() {
-    const { isLoggedIn, posts } = this.state;
+    const { isLoggedIn, posts, currentUser } = this.state;
 
     return (
       <>
         <Router>
-          <NavBar isLoggedIn={isLoggedIn} />
+          <NavBar isLoggedIn={isLoggedIn} handleClick={this.handleLogOut} />
           <Switch>
             <Route exact path="/">
               <Posts posts={posts} deleteHandler={this.deletePost} />
             </Route>
             <Route exact path="/add-post">
-              <PostCreator clickHandler={this.addPost} />
+              <PostCreator
+                clickHandler={this.addPost}
+                currentUserName={currentUser.name}
+              />
             </Route>
 
             <Route path="/login">
-              <LogIn />
+              <LogIn handleLogInClick={this.handleLogIn} />
             </Route>
           </Switch>
         </Router>
