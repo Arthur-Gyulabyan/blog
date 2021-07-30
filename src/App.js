c7984664685c1826b10c5148dc43955dbae837aa
Arthur <gyulabyana@gmail.com>
2021-07-31 03:27:13 +0400

    feat(App): import notistack

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { SnackbarProvider } from 'notistack';
 import Main from './components/Main/Main';
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <div className="App">
-        <Main />
-      </div>
+      <SnackbarProvider maxSnack={3}>
+        <div className="App">
+          <Main />
+        </div>
+      </SnackbarProvider>
     </>
   );
 }
